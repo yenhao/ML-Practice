fb694c4e4c1654171956209ba19e89ffa8b596f4
yenhao <yenhao0218@gmail.com>
2017-07-25 23:17:21 +0800

    Solving some unknown concept

--- Tensorflow/nn/begin_mnist.py
+++ Tensorflow/nn/begin_mnist.py
@@ -11,9 +11,16 @@ Define Code
 
 Set the input nodes
 
-Not understand:
 Represent this as a 2-D tensor of floating-point numbers, with a shape [None, 784].
 (Here None means that a dimension can be of any length.)
+
+The input images x will consist of a 2d tensor of floating point numbers.
+Here we assign it a shape of [None, 784], where 784 is the dimensionality of a single flattened 28 by 28 pixel MNIST image,
+ and None indicates that the first dimension, corresponding to the batch size, can be of any size. 
+"
+
+
+
 """
 x = tf.placeholder(tf.float32, [None, 784])
 
@@ -41,6 +48,8 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 Define Code
 
 Set the correct answer (label) input nodes
+The target output classes y_ will also consist of a 2d tensor,
+ where each row is a one-hot 10-dimensional vector indicating which digit class (zero through nine) the corresponding MNIST image belongs to.
 """
 y_ = tf.placeholder(tf.float32, [None, 10])
 
@@ -54,19 +63,26 @@ reduction_indices=[1]
 """
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
+# cross_entropy = tf.reduce_mean(
+    # tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 """
 Define Code
 
 Define the optimizer and the best critia (minimize the cross_entropy function)
 """
-train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 """
 launch model
 
 Not understand:
 Difference between Session & InteractiveSession
+
+Explaination
+
+InteractiveSession class, which makes TensorFlow more flexible about how you structure your code. It allows you to interleave operations which build a computation graph with ones that run the graph. This is particularly convenient when working in interactive contexts like IPython. If you are not using an InteractiveSession, then you should build the entire computation graph before starting a session and launching the graph.
+
 """
 sess = tf.InteractiveSession()
 
@@ -74,8 +90,12 @@ sess = tf.InteractiveSession()
 Execute Code
 
 Initialize the variables
+
+Before Variables can be used within a session, they must be initialized using that session. This step takes the initial values (in this case tensors full of zeros) that have already been specified, and assigns them to each Variable. This can be done for all Variables at once
 """
 tf.global_variables_initializer().run()
+
+# sess.run(tf.global_variables_initializer()) # another usage
 
 """
 Execute Code
@@ -114,8 +134,11 @@ To get the accuracy
 """
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
+# print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})) # function same as previous one
+
 """
 Get about 92% accuracy in document
 
-But I only get 90%
+But I only get 90% in second cross entropy
+And get ~92% in first cross entropy
 """

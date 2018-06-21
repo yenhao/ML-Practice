ae5c86382bb09767c4e3e005a58dca997b174ebc
yenhao <yenhao0218@gmail.com>
2018-06-22 00:46:14 +0800

    add l2 regularization and tensorboard

--- AutoEncoder/mnist.py
+++ AutoEncoder/mnist.py
@@ -23,8 +23,10 @@ def load_mnist():
     return train_data, valid_data, test_data, input_shape, label_shape
 
 class AutoEncoder(object):
-    def __init__(self, n_features, layers=[], code_nodes=2, learning_rate= 0.013):
+    def __init__(self, n_features, layers=[], code_nodes=2, learning_rate= 0.013, alpha=0.1):
         self.code_nodes = code_nodes
+
+        self.regularizer = tf.contrib.layers.l2_regularizer(scale=alpha)
 
         self.graph = tf.Graph() # initialize new graph
         self.model(n_features, learning_rate, layers)
@@ -35,6 +37,7 @@ class AutoEncoder(object):
         Build model with tensorflow
         :return: Nothing
         """
+
         with self.graph.as_default():
             self.X = tf.placeholder(tf.float32, shape=(None, n_features), name="X_Input")
 
@@ -46,6 +49,7 @@ class AutoEncoder(object):
                                              nodes,
                                              activation=tf.nn.relu,
                                              kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                             kernel_regularizer= self.regularizer,
                                              name="FC_Encode_Layer_" + str(nodes))
 
             # encode layer
@@ -53,6 +57,7 @@ class AutoEncoder(object):
                                   encode_nodes,
                                   activation=tf.nn.relu,
                                   kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                  kernel_regularizer= self.regularizer,
                                   name="Code")
 
             layer_input = self.code
@@ -63,20 +68,33 @@ class AutoEncoder(object):
                                              nodes,
                                              activation=tf.nn.relu,
                                              kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                             kernel_regularizer= self.regularizer,
                                              name="FC_Decode_Layer_" + str(nodes))
 
             self.y_ = tf.layers.dense(layer_input,
                                   784,
                                   activation=tf.nn.sigmoid,
                                   kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                  kernel_regularizer=self.regularizer,
                                   name="y_pred")
 
-            self.loss = tf.reduce_mean(tf.pow(self.y_ - self.X, 2))  # mean square error
+            self.mse_loss = tf.reduce_mean(tf.pow(self.y_ - self.X, 2)) # mean square error
+            self.l2_loss = tf.losses.get_regularization_loss() # l2 loss
+
+            self.loss = self.mse_loss + self.l2_loss/(1+self.l2_loss) # final loss
+
+            tf.summary.scalar('mse_loss', self.mse_loss)
+            tf.summary.scalar('l2_loss', self.l2_loss)
+            tf.summary.scalar('total_loss', self.loss)
+
             self.optimizer = tf.train.AdamOptimizer(learning_rate)
 
-            self.train_op = self.optimizer.minimize(self.loss) # learning goal
+            self.train_op = self.optimizer.minimize(self.mse_loss) # learning goal
 
             self.init_op = tf.global_variables_initializer() # initialization
+
+            self.merged = tf.summary.merge_all()
+
 
     def fit(self, X, X_valid, X_test=None, epochs = 10, batch_size=32, total_sample=1000):
         """
@@ -90,6 +108,8 @@ class AutoEncoder(object):
         :param total_sample: numbers of sample for training samples
         :return: nothing
         """
+        self.writer = tf.summary.FileWriter("TensorBoard/", graph=self.graph)
+
         self.sess.run(self.init_op) # initializing
 
         for epoch in range(epochs):
@@ -102,6 +122,9 @@ class AutoEncoder(object):
                 if i % 250 == 0:
                     print("Train [%d/%d] loss = %9.4f" % (i+1, rounds, loss))
 
+                    summary = self.sess.run(self.merged, feed_dict={self.X: X_batch[0]})
+                    self.writer.add_summary(summary=summary, global_step=(epoch+1)*rounds+i+1)
+
             # validation
             [loss] = self.sess.run([self.loss], feed_dict={self.X: X_valid.images})
             print("Valid loss = %9.4f" % loss)
@@ -110,6 +133,8 @@ class AutoEncoder(object):
             if X_test:
                 [loss] = self.sess.run([self.loss], feed_dict={self.X: X_test.images})
                 print("Test loss = %9.4f" % loss)
+
+
 
 
     def predict(self, X):
@@ -128,18 +153,18 @@ if __name__ == '__main__':
     layer_nodes = [512, 256, 128, 64]
     encode_nodes = 10
 
-    auto_encoder = AutoEncoder(input_shape, layer_nodes, encode_nodes, learning_rate=0.00017)
+    auto_encoder = AutoEncoder(input_shape, layer_nodes, encode_nodes, learning_rate=0.00017, alpha=0.01)
 
-    auto_encoder.fit(train_data, valid_data, test_data, total_sample=train_data.images.shape[0], epochs= 15)
+    auto_encoder.fit(train_data, valid_data, test_data, total_sample=train_data.images.shape[0], epochs= 15, batch_size=8)
 
-    figures = 10
+    figures = 15
 
     # set figures
-    fig, axes = plt.subplots(2, figures, figsize=(figures*2, 3), # set size and nums of figures
+    fig, axes = plt.subplots(2, figures, figsize=(figures*1.1, 3), # set size and nums of figures
                              subplot_kw={'xticks': [], 'yticks': []}) # remove the ticks
     fig.subplots_adjust(hspace=0.3, wspace=0.05)
 
-    fig.suptitle("MNIST Auto Encoder (Code Size="+str(encode_nodes)+")") # title for whole figure
+    fig.suptitle("MNIST Auto Encoder (Code Size="+str(encode_nodes)+")", size=15) # title for whole figure
 
     for i in range(figures):
         img = np.reshape(test_data.images[i, :], (28, 28)) # re-shape to image like
